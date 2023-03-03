78ecd0682d19fa635c1fe41cdea44ca947f12f20
deepak-939215 <mbsdeepak2@gmail.com>
2023-03-03 10:28:57 +0530

    Version 1.0.1 Removed Console Messages

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
     socket.on('new user', (username) => {
         users[socket.id] = username;
         io.emit('update users', users);
-        console.log(`${username} connected`);
+        // console.log(`${username} connected`);
     });
 
     socket.on('chat message', (msg) => {
@@ -28,16 +28,16 @@ io.on('connection', (socket) => {
         const timestamp = date.toLocaleString();
         messages.push({ user: users[socket.id], message: msg, time: timestamp });
         io.emit('chat message', { user: users[socket.id], message: msg, time: timestamp });
-        console.log(`${users[socket.id]}: ${msg}`);
+        // console.log(`${users[socket.id]}: ${msg}`);
     });
 
     socket.on('disconnect', () => {
-        console.log(`${users[socket.id]} disconnected`);
+        // console.log(`${users[socket.id]} disconnected`);
         delete users[socket.id];
         io.emit('update users', users);
     });
 });
 
 http.listen(3000, () => {
-    console.log('listening on *:3000');
+    // console.log('listening on *:3000');
 });
